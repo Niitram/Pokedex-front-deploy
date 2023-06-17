a51d7012306524751cf86401708f7fd6c77f58e8
martin marchessi <niitram.mm@gmail.com>
2023-06-17 14:32:55 -0300

    slinter

--- src/Components/PagesList/PagesList.jsx
+++ src/Components/PagesList/PagesList.jsx
@@ -1,60 +1,71 @@
-import React, { useEffect} from 'react'
-import { getAllPokemons } from '../../Redux/actions'
-import { useDispatch, useSelector } from 'react-redux';
-import divideArray from './divideArray';
-import styles from './PagesList.module.css'
-import ShowPokemons from '../ShowPokemons/ShowPokemons';
+import React, { useEffect } from "react";
+import { getAllPokemons } from "../../Redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import divideArray from "./divideArray";
+import styles from "./PagesList.module.css";
+import ShowPokemons from "../ShowPokemons/ShowPokemons";
 
-function PagesList({currentPage,setCurrentPage}) {
-    const copyAllPokemons = useSelector(state=>state.copyAllPokemons)
-    
-    const dispatch = useDispatch()
+function PagesList({ currentPage, setCurrentPage }) {
+  const copyAllPokemons = useSelector((state) => state.copyAllPokemons);
 
-    useEffect(()=>{
-        dispatch(getAllPokemons())
-    },[dispatch])
+  const dispatch = useDispatch();
 
-    const arrayWithPages = divideArray(copyAllPokemons)
+  useEffect(() => {
+    dispatch(getAllPokemons());
+  }, [dispatch]);
 
-    const nextPage = () => {
-        setCurrentPage((prev) => prev + 1);
-    };
-    const prevPage = () => {
-        setCurrentPage((prev) => prev - 1);
-    };
-    return (
-        <div className={styles.container}>
-            <div className={styles.containerButtons}>
-                <button className={styles.buttonNext} disabled={currentPage === 1} onClick={prevPage}>
-                    Previous Page
+  const arrayWithPages = divideArray(copyAllPokemons);
+
+  const nextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+  const prevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerButtons}>
+        <button
+          className={styles.buttonNext}
+          disabled={currentPage === 1}
+          onClick={prevPage}
+        >
+          Previous Page
+        </button>
+        {
+          // eslint-disable-next-line array-callback-return
+          arrayWithPages.map((page, index) => {
+            if (arrayWithPages.length - 1) {
+              return (
+                <button
+                  disabled={currentPage === index + 1}
+                  className={styles.buttonNext}
+                  key={index}
+                  onClick={() => {
+                    <ShowPokemons currentPage={setCurrentPage(index + 1)} />;
+                  }}
+                >
+                  {index + 1}
                 </button>
-                {
-                    arrayWithPages.map((page,index) => {
-                        if (arrayWithPages.length-1) {
-                            return (
-                                <button
-
-                                    disabled={currentPage === index+1}
-                                    className={styles.buttonNext}
-                                    key={index}
-                                    onClick={()=>{
-                                        <ShowPokemons currentPage={setCurrentPage(index+1)} />
-                                    }}>
-                                        {index+1}
-                                </button>
-                            );
-                        }
-                    })
-                }
-                <button className={styles.buttonNext} disabled={currentPage === arrayWithPages.length || arrayWithPages.length ===1} onClick={nextPage}>
-                    Next Page
-                </button>
-            </div>
-            <div className={styles.containerCards}>
-                <ShowPokemons currentPage={currentPage} />
-            </div>
-        </div>
-    )
+              );
+            }
+          })
+        }
+        <button
+          className={styles.buttonNext}
+          disabled={
+            currentPage === arrayWithPages.length || arrayWithPages.length === 1
+          }
+          onClick={nextPage}
+        >
+          Next Page
+        </button>
+      </div>
+      <div className={styles.containerCards}>
+        <ShowPokemons currentPage={currentPage} />
+      </div>
+    </div>
+  );
 }
 
-export default PagesList
+export default PagesList;
